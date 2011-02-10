3dcf8af402cfea57dfeffc611afe2eaedfd854cc
Darrin Holst <darrinholst@gmail.com>
2011-02-10 08:29:40 -0600

    version 2 movie mapping

--- lib/edboxra/v2/v2_api.rb
+++ lib/edboxra/v2/v2_api.rb
@@ -1,12 +1,35 @@
+require 'date'
+require 'json'
+
 module Edboxra
   module V2
     class Api < Edboxra::Api
       KEY_REGEX = /rb\.api\.key *= * [',"](.*?)[',"]/
 
       def get_movies
+        resp = RestClient.get("http://www.redbox.com/api/product/js/__titles")
+
+        match = /=\ *(\[.*\])/.match(resp.body)
+        raise "couldn't find movies in #{resp.body}" unless match
+
+        json = JSON.parse(match[1])
+        json.map{|movie| map_movie(movie)}
       end
 
       def add_metadata_to(movie)
+      end
+
+      private
+
+      def map_movie(json)
+        movie = Movie.new
+        movie.id = json["ID"]
+        movie.name = json["name"]
+        movie.release_date = Date.parse(json['release'])
+        movie.image = json["img"]
+        movie.genre_ids = json["genreIDs"]
+        movie.blu_ray = json["fmt"].eql? "2"
+        movie
       end
     end
   end
--- test/unit/v2/test_v2_api.rb
+++ test/unit/v2/test_v2_api.rb
@@ -1,6 +1,34 @@
 require 'test_helper'
 
-class TestV2Api < TestCase
-  MOVIES_HTML = 'Type.registerNamespace(\'rb.Cache\'); rb.Cache.__Title = [{"Cst":"0","ID":3942,"Name":"Red Hill","SortName":"red hill","Img":"RedHill_3942.jpg","Release":"20110125","GenreIDs":[1000,1003],"FormatID":"1","ProductType":"1","Def":"1","Limited":"0"},{"Cst":"0","ID":3956,"Name":"Saw: The Final Chapter","SortName":"saw: the final chapter","Img":"SawVII_3956.jpg","Release":"20110125","GenreIDs":[1009,1020,1093],"FormatID":"1","ProductType":"1","Def":"1","Limited":"0"},{"Cst":"0","ID":4385,"Name":"Takers (BLU-RAY)","SortName":"takers (blu-ray)","Img":"TakersBLURAY_4385.jpg","Release":"20110118","GenreIDs":[1000,1003,1022],"FormatID":"2","ProductType":"1","Def":"1","Limited":"0"}];'
+describe Edboxra::V2::Api do
+  MOVIES_JS = 'var __titles = [{"ID":3942,"soon":"0","name":"Red Hill","SEO":"red-hill","sortName":"red hill","img":"RedHill_3942.jpg","release":"20110125","releaseDays":16,"genreIDs":[1000,1003],"fmt":"1","productType":"1","rating":"R","def":"1","limited":"0"},{"ID":3956,"soon":"0","name":"Saw: The Final Chapter","SEO":"saw--the-final-chapter","sortName":"saw: the final chapter","img":"SawVII_3956.jpg","release":"20110125","releaseDays":16,"genreIDs":[1009,1020,1093],"fmt":"1","productType":"1","rating":"R","def":"1","limited":"0"},{"ID":4385,"soon":"0","name":"Takers (BLU-RAY)","SEO":"takers-blu-ray","sortName":"takers (blu-ray)","img":"TakersBLURAY_4385.jpg","release":"20110118","releaseDays":23,"genreIDs":[1000,1003,1022],"fmt":"2","productType":"1","rating":"R","def":"1","limited":"0"}]'
+
+  before do
+    FakeBox.fakeout_version_2
+    FakeBox.expect_get("http://www.redbox.com/api/product/js/__titles", MOVIES_JS)
+    @api = Edboxra::ApiFactory.create_api
+  end
+
+  it "should get a list of movies" do
+    movies = @api.get_movies
+
+    movies.size.must_equal 3
+
+    red_hill = movies[0]
+    red_hill.id.must_equal 3942
+    red_hill.name.must_equal "Red Hill"
+    red_hill.release_date.to_s.must_equal "2011-01-25"
+    red_hill.image.must_equal "RedHill_3942.jpg"
+    red_hill.genre_ids.must_equal [1000, 1003]
+    red_hill.blu_ray?.must_equal false
+  end
+
+  it "should parse blu ray movies" do
+    movies = @api.get_movies
+
+    takers = movies[2]
+    takers.id.must_equal 4385
+    takers.blu_ray?.must_equal true
+  end
 end
 
